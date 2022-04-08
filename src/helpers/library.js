f8e0049f3efda25d9ba3f3baaffa8c37838e5e37
Craig <97862337+cbarber1984@users.noreply.github.com>
2022-04-07 22:07:46 -0400

    Initial commit.

---
+++ src/helpers/library.js
@@ -0,0 +1,94 @@
+import bike_logo from "../images/bike_logo.png";
+import bike_scene from "../images/bike_scene.png";
+import bike from "../images/bike.png";
+import camping from "../images/camping.png";
+import camping_logo from "../images/camping_logo.png";
+import camping_scene from "../images/camping_scene.png";
+import jam from "../images/jam.png";
+import jamLogo from "../images/jamLogo.png";
+import jamScene from "../images/jamScene.png";
+import introducing_logo from "../images/introducing_logo.png";
+import introducing_scene from "../images/introducing_scene.png";
+import actingMom from "../images/introducing.png";
+import masterwork_logo from "../images/masterwork_logo.png";
+import masterwork_scene from "../images/masterwork_scene.png";
+import masterwork from "../images/masterwork.png";
+import moralis from "../images/moralis.png";
+import moralisLogo from "../images/moralisLogo.png";
+import moralisScene from "../images/moralisScene.png";
+
+export const movies = [
+  {
+    Movie: "https://ipfs.moralis.io:2053/ipfs/Qmdd4YMPcwmXgBFSZVvqZWM1YGm8a6nwkpASms5NBkHTEn",
+    Thumnbnail: bike,
+    Scene: bike_scene,
+    Logo: bike_logo,
+    Name: "Bike Ride",
+    Description:
+      "Shredding some trails with the boys!",
+    Genre: "Action, Family",
+    Actors: "A couple of cool kids",
+    Year: "2021",
+    Duration: "0:44",
+  },
+  {
+    Movie: "https://ipfs.moralis.io:2053/ipfs/QmXUMpff33vCZ8vavkK3C7Q1Z65SWDwmxNBWCvbKUzVHLi",
+    Thumnbnail: masterwork,
+    Scene: masterwork_scene,
+    Logo: masterwork_logo,
+    Name: "A Mom's Masterwork",
+    Description: "Bio feature on a wonderful local actress.",
+    Genre: "Family, Biography",
+    Actors: "Craig, Karissa",
+    Year: "2018",
+    Duration: "2:43",
+  },
+  {
+    Movie: "https://ipfs.moralis.io:2053/ipfs/QmTmw2LsyD9UvEfoH9SAukoceeqhHwgeZU9SXKouTVbNd9",
+    Thumnbnail: camping,
+    Scene: camping_scene,
+    Logo: camping_logo,
+    Name: "Camping with Dad",
+    Description: "Primitive camping trip with Dad and two of the kids.",
+    Genre: "Family",
+    Actors: "Dad, Kid 1, Kid 2",
+    Year: "2018",
+    Duration: ":28",
+  },
+  {
+    Movie: "https://ipfs.moralis.io:2053/ipfs/QmWWnyCZwh8RVnamjeLhGmpYM2C3oDr3FzF7zgv5ur24z5/media/0",
+    Thumnbnail: moralis,
+    Scene: moralisScene,
+    Logo: moralisLogo,
+    Name: "Moralis Rap",
+    Description: "Moralis serverless Web3 Dapps, is a beautiful music video that tells a touching story of how anyone can become a Web3 developer if they put their trust in the hands of the musically gifted team at Moralis. ",
+    Genre: "Music Video",
+    Actors: "Ivan Liljeqvist, Filip Martinsson, CTO Larsson",
+    Year: "2021",
+    Duration: "1min 2sec",
+  },
+  {
+    Movie: "https://ipfs.moralis.io:2053/ipfs/QmRBhFWx6ab6snXzmdbnq3nzAcba5tiatLQedsVqfXPnP6",
+    Thumnbnail: actingMom,
+    Scene: introducing_scene,
+    Logo: introducing_logo,
+    Name: "Introducing the Acting Mom",
+    Description: "An awesome woman doing awesome things!",
+    Genre: "Lifestyle",
+    Actors: "Karissa",
+    Year: "2018",
+    Duration: "1:03",
+  },
+  {
+    Movie: "https://ipfs.moralis.io:2053/ipfs/QmSi84pJTjBXmXREN8NMRuRnK9oobYb8QcPZsXUj3GZr12/media/0",
+    Thumnbnail: jam,
+    Scene: jamScene,
+    Logo: jamLogo,
+    Name: "Jam Doughnut",
+    Description: "In this masterpiece a wild Jay, endulges in flavorful jam doughnut. Be warned, this video is most certainly going to leave you craving for some sweets.",
+    Genre: "Silent Mukbang",
+    Actors: "Jay",
+    Year: "2022",
+    Duration: "1min 5sec",
+  },
+];
